ce2544f36acbb1f518f43ba95c0cb84dc34f9559
Benjamin Hildebrandt <benhamin.h9@gmail.com>
2015-04-29 16:24:28 +0200

    Spielbrett erstellt

--- js/spielfeld.js
+++ js/spielfeld.js
@@ -2,35 +2,190 @@
  * Diese Script Datei erstellt das Spielfeld bzw. das Spielbrett
  */
 
-var spielfelder;
-var gewinnfelder;
-var hausfelder;
+var spielfelder = new Array(40);
+var gewinnfelder = new Array(4);
+var hausfelder = new Array(4);
 
-var radius = 0.6;
-var segmente = 32;
+// Variablen und Werte fuer die Spielfelder
+var radius = 0.35;
+var segmente = 50;
 
 /*
  * Erstellt das Spielfeld mit Huetchen im 'Haus'
  */
 function erstelleSpielfeld() {
-  var geometry = new THREE.BoxGeometry(11, 0.1, 11);
-  var material = new THREE.MeshBasicMaterial({
-    color: 0xDCC088
-  });
-  var cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+    intialisiereSpielfelder();
+    var geometry = new THREE.BoxGeometry(11, 0.1, 11);
+    var material = new THREE.MeshBasicMaterial({
+        color: 0xDCC088
+    });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
 }
 
 /*
  * Initialisiert ein Array mit geometrischen Kreisen
+ * Das Spielfeld beginnt mit dem Startplatz der Farbe Rot und
+ * hat insgesamt 40 Felder zu denen noch die Gewinnfelder und die Haus-
+ * felder kommen.
  */
-function intialisiereSpielfelder (){
-  var circleGeometry = new THREE.CircleGeometry(radius, segmente);
-  var material = new THREE.MeshBasicMaterial({
-    color: 0xFAFAD9
-  });
-  
-  for(var i = 0; i < 40; i++){
-    spielfelder[i] = new THREE.Mesh(circleGeometry, material);
-  }
+function intialisiereSpielfelder() {
+    var cylinderGeometry = new THREE.CylinderGeometry(radius, radius, 0.2, segmente);
+    // Material fuer Spielfelder
+    var materialCylinder = new THREE.MeshBasicMaterial({
+        color: 0xFAFAD9
+    });
+    // Materialfarbe fuer Spieler Gelb
+    var materialGelb = new THREE.MeshBasicMaterial({
+        color: 0xFFFC00
+    });
+    // Materialfarbe fuer Spieler Gruen
+    var materialGruen = new THREE.MeshBasicMaterial({
+        color: 0x2EAE00
+    });
+    // Materialfarbe fuer Spieler Blau
+    var materialBlau = new THREE.MeshBasicMaterial({
+        color: 0x2600FF
+    });
+    // Materialfarbe fuer Spieler Rot
+    var materialRot = new THREE.MeshBasicMaterial({
+        color: 0xFF0000
+    });
+
+    /*
+     * Fuegt dem Array spielfelder die Geometrie des Zylinders und der Farbe
+     * der normalen Spielfelder hinzu
+     */
+    for (var i = 0; i < spielfelder.length; i++) {
+        spielfelder[i] = new THREE.Mesh(cylinderGeometry, materialCylinder);
+    }
+
+    /*
+     * Setzt die Position der einzelnen Spielfelder auf dem gesamten Spielbrett
+     * fest und fuegt sie der Szene hinzu
+     */
+    spielfelder[0].position.set(-1, 0, -5);
+    spielfelder[1].position.set(-1, 0, -4);
+    spielfelder[2].position.set(-1, 0, -3);
+    spielfelder[3].position.set(-1, 0, -2);
+    spielfelder[4].position.set(-1, 0, -1);
+    spielfelder[5].position.set(-2, 0, -1);
+    spielfelder[6].position.set(-3, 0, -1);
+    spielfelder[7].position.set(-4, 0, -1);
+    spielfelder[8].position.set(-5, 0, -1);
+    spielfelder[9].position.set(-5, 0, 0);
+    spielfelder[10].position.set(-5, 0, 1);
+    spielfelder[11].position.set(-4, 0, 1);
+    spielfelder[12].position.set(-3, 0, 1);
+    spielfelder[13].position.set(-2, 0, 1);
+    spielfelder[14].position.set(-1, 0, 1);
+    spielfelder[15].position.set(-1, 0, 2);
+    spielfelder[16].position.set(-1, 0, 3);
+    spielfelder[17].position.set(-1, 0, 4);
+    spielfelder[18].position.set(-1, 0, 5);
+    spielfelder[19].position.set(0, 0, 5);
+    spielfelder[20].position.set(1, 0, 5);
+    spielfelder[21].position.set(1, 0, 4);
+    spielfelder[22].position.set(1, 0, 3);
+    spielfelder[23].position.set(1, 0, 2);
+    spielfelder[24].position.set(1, 0, 1);
+    spielfelder[25].position.set(2, 0, 1);
+    spielfelder[26].position.set(3, 0, 1);
+    spielfelder[27].position.set(4, 0, 1);
+    spielfelder[28].position.set(5, 0, 1);
+    spielfelder[29].position.set(5, 0, 0);
+    spielfelder[30].position.set(5, 0, -1);
+    spielfelder[31].position.set(4, 0, -1);
+    spielfelder[32].position.set(3, 0, -1);
+    spielfelder[33].position.set(2, 0, -1);
+    spielfelder[34].position.set(1, 0, -1);
+    spielfelder[35].position.set(1, 0, -2);
+    spielfelder[36].position.set(1, 0, -3);
+    spielfelder[37].position.set(1, 0, -4);
+    spielfelder[38].position.set(1, 0, -5);
+    spielfelder[39].position.set(0, 0, -5);
+
+    // Fuegt die Spielfelder der Szene hinzu
+    for (var i = 0; i < spielfelder.length; i++) {
+        scene.add(spielfelder[i]);
+    }
+
+    /*
+     * Initialisiert Haus- und Gewinnfelder mit dem jeweiligen Material bzw.
+     * der Farbe
+     */
+    for (var i = 0; i < 4; i++) {
+        gewinnfelder[i] = new Array(4);
+        hausfelder[i] = new Array(4);
+
+        if( i === 0){
+            for (var j = 0; j < 4; j++) {
+                gewinnfelder[i][j] = new THREE.Mesh(cylinderGeometry, materialRot);
+                hausfelder[i][j] = new THREE.Mesh(cylinderGeometry, materialRot);
+            }
+        }
+        if (i === 1){
+            for (var j = 0; j < 4; j++) {
+                gewinnfelder[i][j] = new THREE.Mesh(cylinderGeometry, materialBlau);
+                hausfelder[i][j] = new THREE.Mesh(cylinderGeometry, materialBlau);
+            }
+        }
+        if (i === 2){
+            for (var j = 0; j < 4; j++) {
+                gewinnfelder[i][j] = new THREE.Mesh(cylinderGeometry, materialGruen);
+                hausfelder[i][j] = new THREE.Mesh(cylinderGeometry, materialGruen);
+            }
+        }
+        if (i === 3){
+            for (var j = 0; j < 4; j++) {
+                gewinnfelder[i][j] = new THREE.Mesh(cylinderGeometry, materialGelb);
+                hausfelder[i][j] = new THREE.Mesh(cylinderGeometry, materialGelb);
+            }
+        }
+    }
+
+    // Positionen fuer Rot
+    hausfelder[0][0].position.set(-5, 0, -5);
+    hausfelder[0][1].position.set(-4, 0, -5);
+    hausfelder[0][2].position.set(-4, 0, -4);
+    hausfelder[0][3].position.set(-5, 0, -4);
+    gewinnfelder[0][0].position.set(0, 0, -4);
+    gewinnfelder[0][1].position.set(0, 0, -3);
+    gewinnfelder[0][2].position.set(0, 0, -2);
+    gewinnfelder[0][3].position.set(0, 0, -1);
+    // Positionen fuer Blau
+    hausfelder[1][0].position.set(-5, 0, 5);
+    hausfelder[1][1].position.set(-5, 0, 4);
+    hausfelder[1][2].position.set(-4, 0, 4);
+    hausfelder[1][3].position.set(-4, 0, 5);
+    gewinnfelder[1][0].position.set(-4, 0, 0);
+    gewinnfelder[1][1].position.set(-3, 0, 0);
+    gewinnfelder[1][2].position.set(-2, 0, 0);
+    gewinnfelder[1][3].position.set(-1, 0, 0);
+    // Positionen fuer Gruen
+    hausfelder[2][0].position.set(5, 0, 5);
+    hausfelder[2][1].position.set(4, 0, 5);
+    hausfelder[2][2].position.set(4, 0, 4);
+    hausfelder[2][3].position.set(5, 0, 4);
+    gewinnfelder[2][0].position.set(0, 0, 4);
+    gewinnfelder[2][1].position.set(0, 0, 3);
+    gewinnfelder[2][2].position.set(0, 0, 2);
+    gewinnfelder[2][3].position.set(0, 0, 1);
+    // Positionen fuer Gelb
+    hausfelder[3][0].position.set(5, 0, -5);
+    hausfelder[3][1].position.set(5, 0, -4);
+    hausfelder[3][2].position.set(4, 0, -4);
+    hausfelder[3][3].position.set(4, 0, -5);
+    gewinnfelder[3][0].position.set(4, 0, 0);
+    gewinnfelder[3][1].position.set(3, 0, 0);
+    gewinnfelder[3][2].position.set(2, 0, 0);
+    gewinnfelder[3][3].position.set(1, 0, 0);
+    
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            scene.add(gewinnfelder[i][j]);
+            scene.add(hausfelder[i][j]);
+        }
+    }
 }
