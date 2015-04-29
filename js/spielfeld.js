6a10eb9648d08f60467d2ddae81057d8772d25ce
Benjamin Hildebrandt <benhamin.h9@gmail.com>
2015-04-29 16:44:29 +0200

    Fehler behoben

--- js/spielfeld.js
+++ js/spielfeld.js
@@ -118,7 +118,7 @@ function intialisiereSpielfelder() {
     for (var i = 0; i < 4; i++) {
         gewinnfelder[i] = new Array(4);
         hausfelder[i] = new Array(4);
-
+        
         if( i === 0){
             for (var j = 0; j < 4; j++) {
                 gewinnfelder[i][j] = new THREE.Mesh(cylinderGeometry, materialRot);
@@ -150,37 +150,40 @@ function intialisiereSpielfelder() {
     hausfelder[0][1].position.set(-4, 0, -5);
     hausfelder[0][2].position.set(-4, 0, -4);
     hausfelder[0][3].position.set(-5, 0, -4);
-    gewinnfelder[0][0].position.set(0, 0, -4);
-    gewinnfelder[0][1].position.set(0, 0, -3);
-    gewinnfelder[0][2].position.set(0, 0, -2);
-    gewinnfelder[0][3].position.set(0, 0, -1);
+    gewinnfelder[0][0].position.set(-4, 0, 0);
+    gewinnfelder[0][1].position.set(-3, 0, 0);
+    gewinnfelder[0][2].position.set(-2, 0, 0);
+    gewinnfelder[0][3].position.set(-1, 0, 0);
+    
     // Positionen fuer Blau
     hausfelder[1][0].position.set(-5, 0, 5);
     hausfelder[1][1].position.set(-5, 0, 4);
     hausfelder[1][2].position.set(-4, 0, 4);
     hausfelder[1][3].position.set(-4, 0, 5);
-    gewinnfelder[1][0].position.set(-4, 0, 0);
-    gewinnfelder[1][1].position.set(-3, 0, 0);
-    gewinnfelder[1][2].position.set(-2, 0, 0);
-    gewinnfelder[1][3].position.set(-1, 0, 0);
+    gewinnfelder[1][0].position.set(0, 0, 4);
+    gewinnfelder[1][1].position.set(0, 0, 3);
+    gewinnfelder[1][2].position.set(0, 0, 2);
+    gewinnfelder[1][3].position.set(0, 0, 1);
+    
     // Positionen fuer Gruen
     hausfelder[2][0].position.set(5, 0, 5);
     hausfelder[2][1].position.set(4, 0, 5);
     hausfelder[2][2].position.set(4, 0, 4);
     hausfelder[2][3].position.set(5, 0, 4);
-    gewinnfelder[2][0].position.set(0, 0, 4);
-    gewinnfelder[2][1].position.set(0, 0, 3);
-    gewinnfelder[2][2].position.set(0, 0, 2);
-    gewinnfelder[2][3].position.set(0, 0, 1);
+    gewinnfelder[2][0].position.set(4, 0, 0);
+    gewinnfelder[2][1].position.set(3, 0, 0);
+    gewinnfelder[2][2].position.set(2, 0, 0);
+    gewinnfelder[2][3].position.set(1, 0, 0);
+    
     // Positionen fuer Gelb
     hausfelder[3][0].position.set(5, 0, -5);
     hausfelder[3][1].position.set(5, 0, -4);
     hausfelder[3][2].position.set(4, 0, -4);
     hausfelder[3][3].position.set(4, 0, -5);
-    gewinnfelder[3][0].position.set(4, 0, 0);
-    gewinnfelder[3][1].position.set(3, 0, 0);
-    gewinnfelder[3][2].position.set(2, 0, 0);
-    gewinnfelder[3][3].position.set(1, 0, 0);
+    gewinnfelder[3][0].position.set(0, 0, -4);
+    gewinnfelder[3][1].position.set(0, 0, -3);
+    gewinnfelder[3][2].position.set(0, 0, -2);
+    gewinnfelder[3][3].position.set(0, 0, -1);
     
     for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
